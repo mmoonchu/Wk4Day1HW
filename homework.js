4e97449ac295f08f815be87dedd7b930c3a620fc
Moon Chu <mmoonchu2@gmail.com>
2023-10-24 22:57:04 -0400

    created person class

--- homework.js
+++ homework.js
@@ -14,6 +14,48 @@ class Hamster {
         return this.price;
     }
 }
-
 const jerry = new Hamster('moon', 'jerry');
 console.log(jerry.getPrice());
+
+class Person {
+    constructor(name, age = 0, height = 0, weight = 0, mood = 0, hamsters = [], bankAccount = 0) {
+        this.name = name,
+        this.age = age,
+        this.height = height,
+        this.weight = weight,
+        this.mood = mood,
+        this.hamsters = hamsters,
+        this.bankAccount = bankAccount
+    }
+    getName() {
+        return this.name;
+    }
+    getAge() {
+        return this.age;
+    }
+    getWeight() {
+        return this.weight;
+    }
+    greet() {
+        console.log(`Hi, my name is ${this.name}.`);
+    }
+    eat() {
+        this.weight++;
+        this.mood++;
+    }
+    exercise() {
+        this.weight--;
+    }
+    ageUp() {
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount += 10;
+    }
+    buyHamster(hamster) {
+        this.hamsters.push(hamster);
+        this.mood += 10;
+        this.bankAccount -= hamster.getPrice();
+    }
+}

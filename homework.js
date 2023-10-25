a3108a9e885edd1fac4107995def17522be3e712
Moon Chu <mmoonchu2@gmail.com>
2023-10-24 21:50:21 -0400

    created hamster class

---
+++ homework.js
@@ -0,0 +1,19 @@
+class Hamster {
+    constructor(owner = '', name, price = 15) {
+        this.owner = owner;
+        this.name = name;
+        this.price = price;
+    }
+    wheelRun() {
+        console.log('squeak squeak');
+    }
+    eatFood() {
+        console.log('nibble nibble');
+    }
+    getPrice() {
+        return this.price;
+    }
+}
+
+const jerry = new Hamster('moon', 'jerry');
+console.log(jerry.getPrice());
